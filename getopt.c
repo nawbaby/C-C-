7dea0913b565a021c61a64b0de746f71bc908f22
nawbaby <2604552690@qq.com>
2020-08-10 18:06:48 +0800

    添加README.md

---
+++ getopt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<unistd.h>
+#include<time.h>
+　　int main(int argc, char *argv[])
+{
+	clock_t t1,t2;
+	t1 = clock();
+	sleep(5);
+}
--- kanbudong.c
+++ kanbudong.c
@@ -1,24 +1,28 @@
-#include <stdio.h>
-#include <string.h>
+ 1 #include <stdio.h>
+ 2 #include <string.h>
+ 3 
+ 4 
+ 5 const char g_ip[32] = "123456789";
+ 6 int func1(const char *ip)
+ 7 {
+ 8         printf("ip:%s\n",ip);
+ 9         printf("ip size:%d\n",sizeof(ip));
+10         if(*ip == '\0')
+11                 printf("ip is none\n");
+12         printf("g_ip:%s\n",g_ip);
+13         printf("g_ip size:%d\n",sizeof(g_ip));
+14         if(*ip == '\0')
+15                 printf("g_ip is none\n");
+16         return 0;
+17 }
+18 
+19 int main()
+20 {
+21         const char ip[32]="ABCDEFG";
+22         func1(ip);
+23         return 0;
+24 }
 
-
-const char g_ip[32] = "123456789";
-int func1(const char *ip)
-{
-printf("ip:%s\n",ip);
-printf("ip size:%d\n",sizeof(ip));
-if(*ip == '\0')
-printf("ip is none\n");
-printf("g_ip:%s\n",g_ip);
-printf("g_ip size:%d\n",sizeof(g_ip));
-if(*ip == '\0')
-printf("g_ip is none\n");
-return 0;
-}
-
-int main()
-{
-const char ip[32]="ABCDEFG";
-func1(ip);
-return 0;
-}
+/*
+	通过函数传递进去的参数并不会依照定义时候的大小 			   
+*/
